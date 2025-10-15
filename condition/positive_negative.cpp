5fb8dc5d9b93955e8cf38e768b9dcf415640dd45
roudramondal <mondalroudra212@gmail.com>
2025-10-16 00:50:43 +0600

    Done

---
+++ condition/positive_negative.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if (n>=0){
+        cout<<"Positive"<<endl;
+    }
+    else {
+        cout<<"Negative"<<endl;
+    }
+    return 0;
+}
