5acccb06a2906b1d61be0e92adc957ea6c37af1f
roudramondal <mondalroudra212@gmail.com>
2025-10-18 00:43:11 +0600

    use nested loop for practice

---
+++ condition/cricket_world_cup.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int x;
+    cin>>x;
+    if (x>=1&&x<=20){
+        if (x>=12){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+    return 0;
+}
