3becfd233a1a1ca6f8534dc2533c056de255e58b
roudramondal <mondalroudra212@gmail.com>
2025-10-18 22:28:56 +0600

    ok done

--- condition/subscribers.cpp
+++ condition/subscribers.cpp
@@ -3,11 +3,28 @@ using namespace std;
 int main(){
     long long n;
     cin>>n;
-    if (n>=0&&n<=1000){
+    if (n<1000){
         cout<<n<<endl;
     }
-    else if (n>=1000&&n<=9999){
+    else if (n<10000){
         cout<<(n/10)*10<<endl;
     }
+    else if (n<100000){
+        cout<<(n/100)*100<<endl;
+    }
+    else if (n<1000000){
+        cout<<(n/1000)*1000<<endl;
+    }
+    else if (n<10000000){
+        cout<<(n/10000)*10000<<endl;
+    }
+    else if (n<100000000){
+        cout<<(n/100000)*100000<<endl;
+    }
+    else {
+        cout<<(n/1000000)*1000000<<endl;
+    }
+
+    return 0;
 
 }
