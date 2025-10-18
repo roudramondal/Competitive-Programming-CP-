51b36032c7dfac3e252e7f6c310065f944d4c4b0
roudramondal <mondalroudra212@gmail.com>
2025-10-18 17:30:51 +0600

    simple condition check use if else

---
+++ condition/air_quality.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    if (x>=0&&x<=99){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
