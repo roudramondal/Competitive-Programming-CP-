a10b8b1c35665cb657f9d980b959881d05c02f46
roudramondal <mondalroudra212@gmail.com>
2025-10-19 19:35:31 +0600

    swap without use 3rd variable

---
+++ condition/swap.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    // without using a 3rd variable
+    swap(a,b);
+    cout<<a<<" "<<b<<endl;
+    return 0;
+}
