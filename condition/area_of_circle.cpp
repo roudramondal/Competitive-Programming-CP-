ea9123bf9c27d6969d5f006ecc9459957ac18abb
roudramondal <mondalroudra212@gmail.com>
2025-10-23 18:31:53 +0600

    Done

---
+++ condition/area_of_circle.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    double r;
+    cin >> r;
+    double sum1 = 3.141592653 * (r * r);
+    cout << fixed << setprecision(9) << sum1 << endl;
+    return 0;
+}
