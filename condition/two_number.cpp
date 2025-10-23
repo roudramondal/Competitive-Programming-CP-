6d6e8da12dbad64233669b2ca5f6e5edf4cb6772
roudramondal <mondalroudra212@gmail.com>
2025-10-23 18:15:51 +0600

    use floor,ceil and round function

---
+++ condition/two_number.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+    cout << "floor " << a << " / " << b << " = " << floor(double(a / b)) << endl;
+    cout << "ceil " << a << " / " << b << " = " << ceil(double(a / b)) << endl;
+    cout << "round " << a << " / " << b << " = " << round(double(a / b)) << endl;
+
+    return 0;
+}
