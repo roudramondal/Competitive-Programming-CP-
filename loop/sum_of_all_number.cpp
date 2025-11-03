ba0f631a7a3bbd5890b4c7034a10531956d59871
roudramondal <mondalroudra212@gmail.com>
2025-11-03 11:37:13 +0600

    taking input from the user

--- loop/sum_of_all_number.cpp
+++ loop/sum_of_all_number.cpp
@@ -3,9 +3,14 @@ using namespace std;
 int main()
 {
     int sum = 0;
-    for (int i = 1; i <= 100; i++)
-    {
-        sum += i;
+    // for (int i = 1; i <= 100; i++)
+    // {
+    //     sum += i;
+    // }
+    int n;
+    cin>>n;
+    for (int i=1;i<=n;i++){
+        sum+=i;
     }
     cout << sum << endl;
     return 0;
