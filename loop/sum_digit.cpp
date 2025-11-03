821d17c2fa4fe642b8021e4e3608644488015b91
roudramondal <mondalroudra212@gmail.com>
2025-11-03 12:55:30 +0600

    Done

--- loop/sum_digit.cpp
+++ loop/sum_digit.cpp
@@ -2,16 +2,26 @@
 using namespace std;
 int main()
 {
-    int x;
-    cin >> x;
-    int sum = 0;
-    int ans = 0;
-    for (int i=0;;i++){
-        sum+=i;
-        if (sum>=x){
-            ans=i;
-            break;
-        }
+    // int x;
+    // cin >> x;
+    // int sum = 0;
+    // int ans = 0;
+    // for (int i=0;;i++){
+    //     sum+=i;
+    //     if (sum>=x){
+    //         ans=i;
+    //         break;
+    //     }
+    // }
+    // another way to 
+
+    int i=10;
+    while (i>0)
+    {
+        cout<<i<<endl;
+        i--;
     }
-    cout<<ans<<endl;
+    
+    // cout<<ans<<endl;
+    return 0;
 }
