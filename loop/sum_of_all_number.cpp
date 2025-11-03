074402c9a1273844a00c129ada3c62dbd9aad50f
roudramondal <mondalroudra212@gmail.com>
2025-11-03 11:34:19 +0600

    sum of all number one to hundred

---
+++ loop/sum_of_all_number.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int sum = 0;
+    for (int i = 1; i <= 100; i++)
+    {
+        sum += i;
+    }
+    cout << sum << endl;
+    return 0;
+}
