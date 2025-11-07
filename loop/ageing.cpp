e7e1e719406dda1c494f980b41d488e012f654e6
roudramondal <mondalroudra212@gmail.com>
2025-11-08 02:24:03 +0600

    i use here while loop and subtraction x

---
+++ loop/ageing.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        int value = x - 10;
+        cout << value << endl;
+    }
+    return 0;
+}
