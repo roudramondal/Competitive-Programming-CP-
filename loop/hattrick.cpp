f2c3fefea848c1186f565217cef1c16e3f7983c8
roudramondal <mondalroudra212@gmail.com>
2025-11-16 22:09:30 +0600

    lambda function to avoid the space that's it

--- loop/hattrick.cpp
+++ loop/hattrick.cpp
@@ -1,46 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
+
         char a, b, c, d, e, f;
         cin >> a >> b >> c >> d >> e >> f;
-        // if (a == 119 || b == 119 || c == 119 || d == 119 || e == 119 || f == 119)
-        // {
-        //     cout << "YES" << endl;
-        // }
-        // else
-        // {
-        //     cout << "NO" << endl;
-        // }
-        if (a=='w'&&b=='w'&&c== 'w'){
-        cout<<"YES"<<endl;
-       }
-       else if (a== 'w'&&b=='w' &&c== 'w'&&d=='w'){
-        cout<<"YES"<<endl;
-       }
-       else if (b=='w'&&c=='w'&&d=='w'){
-        cout<<"YES"<<endl;
-       }
-       else if (b=='w'&&c=='w'&&d== 'w'&&e== 'w'){
-        cout<<"YES"<<endl;
-       }
-       else if (c=='w'&&d=='w'&&e=='w'){
-        cout<<"YES"<<endl;
-       }
-       else if (c=='w'&&d=='w'&&e=='w'&&f=='w'){
-        cout<<"YES"<<endl;
-       }
-       else if (d=='w'&&e=='w'&&f=='w'){
-        cout<<"YES"<<endl;
-       }
-       else
-       {
-        cout<<"NO"<<endl;
-       }
+
+        auto isW = [](char x){ return x=='W' || x=='w';};
+
+        if ((isW(a) && isW(b) && isW(c)) ||
+            (isW(b) && isW(c) && isW(d)) ||
+            (isW(c) && isW(d) && isW(e)) ||
+            (isW(d) && isW(e) && isW(f))) 
+        {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
