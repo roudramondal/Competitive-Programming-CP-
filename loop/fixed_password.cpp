8339cde084b15404f7b4edcf77e17ca70c201d37
roudramondal <mondalroudra212@gmail.com>
2025-11-27 01:37:07 +0600

    use while loop check condition if true go to the loop

---
+++ loop/fixed_password.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    while(true){
+        cin>>n;
+        if (n==1999){
+            cout<<"Correct"<<endl;
+            break;
+        }
+        else{
+            cout<<"Wrong"<<endl;
+            continue;
+        }
+    }
+    return 0;
+}
