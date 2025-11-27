b040fd1c75805e24d63887bbb6898749b616b8f2
roudramondal <mondalroudra212@gmail.com>
2025-11-27 22:58:54 +0600

    if condition  use for find two digit are same

--- loop/nested_loop/tree_plate.cpp
+++ loop/nested_loop/tree_plate.cpp
@@ -3,14 +3,22 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+
+    int count=0;
     for (int i=1;i<=n;i++){
-        for(int j=i+1;j<=n;j++){
-            for (int k=j+1;k<=n;k++){
+        for(int j=1;j<=n;j++){
+            for (int k=1;k<=n;k++){
+               // this part show two digit are same 
+               if (i==j&&j!=k)count++;
+               if(i==k&&i!=j)count++;
+               if (j==k&&i!=j)count++;
                 cout<<"( "<<i<<","<<j<<","<<k<<")";
             }
            
         }
-        //  cout<<endl;
+         cout<<endl;
     }
+
+    cout<<count<<endl;
     return 0;
 }
