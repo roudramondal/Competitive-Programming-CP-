19ee2542e6ca499a3d07081724713b1d3079bbc9
roudramondal <mondalroudra212@gmail.com>
2025-11-29 22:15:02 +0600

    second loop work for find digit and each digit seperate  and  multiply with power and sum the value then condition check

--- loop/nested_loop/armstrong.cpp
+++ loop/nested_loop/armstrong.cpp
@@ -1,15 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    int count=0;
-    int m=n;
+    cin >> n;
+    int count = 0;
+    int m = n;
     do
     {
         count++;
-        m/=10;
-    } while (m>0);
-    cout<<count<<endl;
-    
+        m /= 10;
+    } while (m > 0);
+    // cout << count << endl;
+    int sum = 0;
+    for (int i = n; i > 0; i /= 10)
+    {
+        int d = i % 10;
+        int power = 1;
+        for (int j = 1; j <= count; j++)
+        {
+            power *= d;
+        }
+
+        sum+=power;
+    }
+
+    if (sum==n){
+        cout<<"Armstrong Number"<<endl;
+    }
+    else{
+        cout<<"Not Armstrong Number"<<endl;
+    }
+    return 0;
 }
