44b2c0c529a6846ce7377fefa11b6af5386b3548
roudramondal <mondalroudra212@gmail.com>
2025-11-29 22:19:56 +0600

    find 1 to n number armstrong number

--- loop/nested_loop/armstrong.cpp
+++ loop/nested_loop/armstrong.cpp
@@ -2,34 +2,67 @@
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
-    int count = 0;
-    int m = n;
-    do
+    int c;
+    cin >> c;
+    // int count = 0;
+    // int m = n;
+    // do
+    // {
+    //     count++;
+    //     m /= 10;
+    // } while (m > 0);
+    // // cout << count << endl;
+    // int sum = 0;
+    // for (int i = n; i > 0; i /= 10)
+    // {
+    //     int d = i % 10;
+    //     int power = 1;
+    //     for (int j = 1; j <= count; j++)
+    //     {
+    //         power *= d;
+    //     }
+
+    //     sum+=power;
+    // }
+
+    // if (sum==n){
+    //     cout<<"Armstrong Number"<<endl;
+    // }
+    // else{
+    //     cout<<"Not Armstrong Number"<<endl;
+    // }
+    
+
+    // this part shows only 1 to n print how many armstrong number has
+    for (int n = 1; n <= c; n++)
     {
-        count++;
-        m /= 10;
-    } while (m > 0);
-    // cout << count << endl;
-    int sum = 0;
-    for (int i = n; i > 0; i /= 10)
-    {
-        int d = i % 10;
-        int power = 1;
-        for (int j = 1; j <= count; j++)
+        int count = 0;
+        int m = n;
+        do
         {
-            power *= d;
+            count++;
+            m /= 10;
+        } while (m > 0);
+        // cout << count << endl;
+        int sum = 0;
+        for (int i = n; i > 0; i /= 10)
+        {
+            int d = i % 10;
+            int power = 1;
+            for (int j = 1; j <= count; j++)
+            {
+                power *= d;
+            }
+
+            sum += power;
         }
 
-        sum+=power;
-    }
-
-    if (sum==n){
-        cout<<"Armstrong Number"<<endl;
-    }
-    else{
-        cout<<"Not Armstrong Number"<<endl;
+        if (sum == n)
+        {
+            // cout << "Armstrong Number" << endl;
+            cout<<n<<" ";
+        }
+       
     }
     return 0;
 }
