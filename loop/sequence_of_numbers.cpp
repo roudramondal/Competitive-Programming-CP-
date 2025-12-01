6aa81585925818a73c51e3ebe783d63b4ea8fd13
roudramondal <mondalroudra212@gmail.com>
2025-12-01 16:28:34 +0600

    if i declare sum inside loop then never happen this problem

--- loop/sequence_of_numbers.cpp
+++ loop/sequence_of_numbers.cpp
@@ -1,25 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int m,n;
-    
-    int sum=0;
-   while (true)
-   {
-     cin>>m>>n;
-     if (m>n) swap(m,n);
-     if (n>0 && m>0){
-        for (int i=m;i<=n;i++){
-            sum+=i;
-            cout<<i<<" ";
+int main()
+{
+    int m, n;
+    while (true)
+    {
+        cin >> m >> n;
+        if (m <= 0 || n <= 0)break;
+        if (m > n) swap(m, n);
+        int sum = 0;
+        for (int i = m; i <= n; i++)
+        {
+            cout << i << " ";
+            sum += i;
         }
-        cout<<"sum = "<<sum<<endl;
-        continue;
+        cout << "sum =" << sum << endl;
     }
-    else{
-        break;
-    }
-   }
-   
+
     return 0;
 }
