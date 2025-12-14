2f757e96b95ca86e704a440c0cc7a6349f59fccc
roudramondal <mondalroudra212@gmail.com>
2025-12-14 23:12:07 +0600

    first array code

---
+++ Array/array1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[3];
+    arr[0]=1;
+    arr[1]=4;
+    arr[2]=50;
+
+    cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2];
+    return 0;
+}
