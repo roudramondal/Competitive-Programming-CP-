531ef0aad2307b6424a387763a4c9dbefc7d84c9
roudramondal <mondalroudra212@gmail.com>
2025-12-14 23:18:25 +0600

    useing loop output display

--- Array/array1.cpp
+++ Array/array1.cpp
@@ -6,6 +6,11 @@ int main(){
     arr[1]=4;
     arr[2]=50;
 
-    cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2];
+    // cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2];
+    // using loop
+
+    for (int i=0;i<3;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
