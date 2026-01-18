6c29cb9c32fd6df25fd90cf8135bece3f2639af8
roudramondal <mondalroudra212@gmail.com>
2026-01-18 23:15:07 +0600

    Done

---
+++ Array/cut.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // output
+    for (int i = n - k; i <= n - 1; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    for (int i = 0; i <= n - k - 1; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
