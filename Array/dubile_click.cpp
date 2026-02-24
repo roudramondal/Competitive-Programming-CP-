66cd324773543a8a359670727cc790c43cde23ef
roudramondal <mondalroudra212@gmail.com>
2026-02-24 19:55:45 +0600

    use array loop and condition also subtraction

---
+++ Array/dubile_click.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, d;
+    cin >> n >> d;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int ans = -1;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int x = arr[i], x2 = arr[i+1];
+        //    cout<<value<<" ";
+        if (x2 - x <= d)
+        {
+            ans = x2;
+            break;
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
