c3efd6263c5b753ef5fee885bbb957f0d55d1a83
roudramondal <mondalroudra212@gmail.com>
2026-02-27 21:34:22 +0600

    Done

--- Array/climbing.cpp
+++ Array/climbing.cpp
@@ -9,17 +9,19 @@ int main()
     {
         cin >> arr[i];
     }
-// output part 
-    for (int i =0;i<n;i++){
-        if (arr[i]<=arr[i+1]){
-
-            int value = arr[i+1]++;
-            cout<<value<<endl;
+    // output part
+    int ans = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > arr[i-1])
+        {
+            ans = arr[i];
+           
+        }
+        else{
             break;
         }
-        // else{
-        //     arr[i+1]++;
-        //     cout<<arr[i+1]<<endl;
-        // }
     }
+    cout << ans << endl;
+    return 0;
 }
