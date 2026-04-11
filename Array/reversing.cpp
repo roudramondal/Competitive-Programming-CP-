0daef9e02e717920d03ab8a6a7a0de6fbdee74df
roudramondal <mondalroudra212@gmail.com>
2026-04-11 13:19:40 +0600

    Done it just use loop for output

--- Array/reversing.cpp
+++ Array/reversing.cpp
@@ -1,14 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main (){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for (int i = 1;i<=n;i++){
-        cin>>arr[i];
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
     }
-    for (int i=n;i>=1;i--){
-        cout<<arr[i]<<" ";
+    for (int i = n; i >= 1; i--)
+    {
+        cout << arr[i] << " ";
     }
     return 0;
 }
