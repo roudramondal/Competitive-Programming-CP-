8d0ea89f72c74aa4f8d56bba2356708a59c09ff6
roudramondal <mondalroudra212@gmail.com>
2026-05-13 22:07:46 +0600

    use here frequency array

---
+++ Array/equlize_array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int freq[101];
+    for (int i = 1; i <= 100; i++)
+    {
+        freq[i] = 0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        freq[arr[i]]++;
+    }
+    int max = 0;
+    for (int i = 1; i <= 100; i++)
+    {
+        if (freq[i] > max)
+        {
+            max = freq[i];
+        }
+    }
+    cout << n - max << endl;
+    return 0;
+}
