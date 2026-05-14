3ab339c4d9dfc3ca02cc14595949b0484007e0fd
roudramondal <mondalroudra212@gmail.com>
2026-05-14 16:56:09 +0600

    taking input from use and show output

---
+++ 2D Array/input_output.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int arr[4][3];
+    // taking input
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    // output
+    cout << "Output Part" << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
