41f5777d50f1631f30d9b98e30f1f93638cbeb5f
roudramondal <mondalroudra212@gmail.com>
2026-05-14 17:48:15 +0600

    sum of all array

---
+++ 2D Array/sum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            sum += arr[i][j];
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
