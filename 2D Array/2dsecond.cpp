82329b0ff63d200645d49b151a8f21b9f33cb0ad
roudramondal <mondalroudra212@gmail.com>
2026-05-14 16:34:18 +0600

    value insert in 2d array and output

---
+++ 2D Array/2dsecond.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[2][2] = {{1, 1}, {1, 2}};
+
+    cout << arr[0][0] << " " << arr[0][1] << " "<< arr[1][0] << " " << arr[1][1];
+
+    return 0;
+}
