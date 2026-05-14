1b5d2c31893de38ef6a3509f5bf7e6eb9c713783
roudramondal <mondalroudra212@gmail.com>
2026-05-14 16:21:43 +0600

    start 2d array

---
+++ 2D Array/2dfirst.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[3][7];
+    // how to insert value in 2d array 
+    // arr[1][3] = 10
+    
+    // take input from use in 2d array
+     cin>> arr[1][3]
+}
