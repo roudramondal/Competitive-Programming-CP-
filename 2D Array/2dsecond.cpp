927acc38e6f0afefae8327ff2376afd8cac96023
roudramondal <mondalroudra212@gmail.com>
2026-05-25 23:17:34 +0600

    commit

--- 2D Array/2dsecond.cpp
+++ 2D Array/2dsecond.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
+    //  initialization of 2d array
     int arr[2][2] = {{1, 1}, {1, 2}};
 
     cout << arr[0][0] << " " << arr[0][1] << " "<< arr[1][0] << " " << arr[1][1];
