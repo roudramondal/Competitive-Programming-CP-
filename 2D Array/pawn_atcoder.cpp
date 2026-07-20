af74d8c4d4fb3c502a7a13ffe33d5cfd15b6efbd
roudramondal <mondalroudra212@gmail.com>
2026-07-20 22:06:23 +0600

    commit

---
+++ 2D Array/pawn_atcoder.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int h, w;
+    cin >> h >> w;
+
+    char arr[h][w];
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            if (arr[i][j] == '#')
+            {
+                count++;
+            }
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
